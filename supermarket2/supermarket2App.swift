84c4a03d47f2a2d8791be7e6d203a4d6f37595ce
pancho63490 <128556102+pancho63490@users.noreply.github.com>
2024-07-24 17:24:29 -0600

    Initial Commit

---
+++ supermarket2/supermarket2App.swift
@@ -0,0 +1,17 @@
+//
+//  supermarket2App.swift
+//  supermarket2
+//
+//  Created by Frank Perez on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct supermarket2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
